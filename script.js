df3dcdb7ce57ba6d2693301934f6b2ecc4018868
Zaw Khing <zawmyokhing@gmail.com>
2022-11-09 23:52:02 +0000

    Complete level 300

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 //You can edit ALL of the code here
+
 const search = document.getElementById("search");
 const totalEpisodes = document.getElementById("total-episodes");
-const matchEpisodes = document.getElementById("matching-ep")
-
+const matchedEpisodes = document.getElementById("matching-ep")
 const allEpisodes =  getAllEpisodes();
 
 function setup() {
-    showAllEpisodes(allEpisodes);
-    populateSelectedEpisode(allEpisodes)
+    displayEpisodes(allEpisodes);
+    selectEpisodes(allEpisodes)
 }
 
-// function makePageForEpisodes(episodeList) {
-//   const rootElem = document.getElementById("root");
-//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-// }
 
-// show all episodes
-function showAllEpisodes(episodes){
+//display all episodes
+function displayEpisodes(episodes){
   const container = document.querySelector(".container");
   container.innerHTML = "";
   totalEpisodes.textContent = allEpisodes.length; 
 
   for(let episode of episodes){
-    const article = document.createElement("Article");
+    const article = document.createElement("article");
   
     const title = document.createElement("h2");
     let formattedEpNumber = ((episode.number.toString()).padStart(2,"0"))// format number to two characters string
@@ -41,42 +37,37 @@ function showAllEpisodes(episodes){
   }
 }
 
-//search episode
+//search episodes
 search.addEventListener('keyup', e => {
   const matchingValue = e.target.value.toUpperCase();
   const matchingEpisodes = allEpisodes.filter((episode)=>{
-        return episode.name.toUpperCase().includes(matchingValue) || episode.summary.toUpperCase().includes(matchingValue);
-  })
-
-  matchEpisodes.innerText = matchingEpisodes.length;
-  showAllEpisodes(matchingEpisodes);
+       return episode.name.toUpperCase().includes(matchingValue) || episode.summary.toUpperCase().includes(matchingValue);
+    })
+  displayEpisodes(matchingEpisodes);
+  matchedEpisodes.innerText = matchingEpisodes.length;
 })
 
-//select options
-function populateSelectedEpisode(episodes){
- const select = document.getElementById("episode-list");
-
- for(let episode of episodes){
-  const option = document.createElement("option");
-  
-  option.setAttribute("value",episode.id);
-  let formattedEpNumber = ((episode.number.toString()).padStart(2,"0"))// format number to two characters string
-  option.innerText = `S0${episode.season}E${formattedEpNumber} - ${episode.name}`;
-  
-  select.appendChild(option);
- }
+//select episodes
+const select = document.getElementById("episode-list");
+function selectEpisodes(episodes){
+   for(let episode of episodes){
+      const option = document.createElement("option");
+      const formattedEpNumber = ((episode.number.toString()).padStart(2,"0"))// format number to two characters string
+      const dropdownOption = `S0${episode.season}E${formattedEpNumber} - ${episode.name}`;
+      option.value = dropdownOption;
+      option.innerText = dropdownOption;
+      
+   select.appendChild(option);
+  }
 }
 
-//populate selected episode
-// select.addEventListener('change', e => {
-//   const selectedEpisode = e.target.value.slice(9)
+ //populate selected episode
+ select.addEventListener('change', e => {
+      const selectedEpisode = e.target.value.slice(9);
+      const selectedEp = allEpisodes.filter((episode)=>{
+           return episode.name === selectedEpisode;
+      }); 
+    displayEpisodes(selectedEp);
+})     
 
-  
-//   const selectedEp = allEpisodes.filter((episode)=>{
-//        return episode.name === selectedEpisode;
-//   })
-
-//   showAllEpisodes(selectedEp);
-// })
-
-window.onload = setup;
+window.onload = setup();
